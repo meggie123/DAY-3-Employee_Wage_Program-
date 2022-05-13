9c572a4b454c1f178c68a04cfd677b883f6fa66c
meggie123 <meghanabura@gmail.com>
2022-05-13 12:43:30 +0530

    Checking Employee Wage For Total Working Condition

---
+++ EmployeewageForTotalWorkingCondition.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.javafundamentals;
+
+public class EmployeewageForTotalWorkingCondition {
+    public static void main(String args[]) {
+
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int Wage_Per_Hr = 20;
+        final int Working_Days = 20;
+
+        int totalWage = 0;
+        for(int day=1;day<=Working_Days;day++)
+        {
+
+            int empType = (int) (Math.random() * 100) % 3;
+            int workingHours = 0;
+            switch (empType)
+            {
+                case FULL_TIME:
+                    workingHours = 8;
+                    break;
+
+                case PART_TIME:
+                    workingHours = 4;
+                    break;
+
+                default:
+            }
+
+            int wage = workingHours * Wage_Per_Hr;
+            System.out.println("Day " + day + " Wage is : " + wage);
+            totalWage += wage;
+
+        }
+        System.out.println("Total wage for a month is " + totalWage);
+    }
+}
+
